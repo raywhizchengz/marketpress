3db4dad5834c7f2de37bf9a908fc5ae9c81af78b
Raychengole97 <raychengole97@gmail.com>
2020-08-17 16:26:57 +0300

    set the api key to hide in the gradle.properties

--- app/src/main/java/com/raychengole/dukayangu/adapters/RecyclerViewAdapter.java
+++ app/src/main/java/com/raychengole/dukayangu/adapters/RecyclerViewAdapter.java
@@ -108,5 +108,4 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
 //    public void searchList(List<Product> newList) {
 //        productList = new ArrayList<>();
-
 }
